e1f09e922e96c6624bc4cc5396cbb4f46c75d8f5
hsynakin <45360362+hsynakin@users.noreply.github.com>
2018-11-26 17:29:37 +0300

    deneme

--- main.go
+++ main.go
@@ -9,4 +9,20 @@ func main() {
 		println("two")
 	}
 
+	if zoo := 1; zoo == 1 {
+		println("çubuk")
+	} else {
+		println("demir  çubuk :)")
+	}
+
+	switch ttoo := 4; {
+	case ttoo == 1:
+		println("one")
+	case ttoo > 2:
+		println("two")
+	}
+
+	for i := 0; i < 3; i++ {
+		println(i)
+	}
 }
