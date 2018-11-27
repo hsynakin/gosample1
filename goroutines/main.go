c38e22af86149ed76ba8b66b7a2149751847ba50
hsynakin <45360362+hsynakin@users.noreply.github.com>
2018-11-27 15:36:12 +0300

    testt

---
+++ goroutines/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"runtime"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(8)
+
+	ch := make(chan string)
+
+	go abcGen(ch)
+	go printer(ch)
+
+	println("bekleme")
+	time.Sleep(100 * time.Microsecond)
+}
+
+func abcGen(ch chan string) {
+	for l := byte('a'); l <= byte('z'); l++ {
+		ch <- string(l)
+	}
+}
+
+func printer(ch chan string) {
+	for {
+		println(<-ch)
+	}
+
+}
