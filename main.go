19c6fa72812ccb6da97d0bfee2b1db5101f19ef8
hsynakin <45360362+hsynakin@users.noreply.github.com>
2018-11-27 14:04:06 +0300

    test

--- main.go
+++ main.go
@@ -1,38 +1,14 @@
 package main
 
 func main() {
-
-	switch foo := 1; foo {
-	case 1:
-		println("one")
-	case 2:
-		println("two")
+	result, tt := add(1, 3, 5, 6, 7)
+	println(tt, result)
+}
+func add(terms ...int) (int, string) {
+	result := 0
+	for _, term := range terms {
+		result += term
 	}
-
-	if zoo := 1; zoo == 1 {
-		println("çubuk")
-	} else {
-		println("demir  çubuk :)")
-	}
-
-	switch ttoo := 4; {
-	case ttoo == 1:
-		println("one")
-	case ttoo > 2:
-		println("two")
-	}
-
-	for i := 0; i < 3; i++ {
-		println(i)
-	}
-	k := 0
-	for {
-		k++
-		println(k)
-
-		if k > 5 {
-			break
-		}
-	}
+	return result, "Sonuç  = "
 
 }
