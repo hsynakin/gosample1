cd4f32c868209073ebb230938afb90d1e164c1ec
hsynakin <45360362+hsynakin@users.noreply.github.com>
2018-11-28 12:27:32 +0300

    1

--- http ex1/main.go
+++ http ex1/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
+type MyHandler struct {
+	http.Handler
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
+	http.Handle("/", new(MyHandler))
+
 	http.ListenAndServe(":8000", nil)
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public/" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
+
 }
